9e9f05e8877762e3fb1a2a4756a053c279b4ea83
Dima <dmytro.polishchuk95@gmail.com>
2021-11-08 02:46:13 +0200

    Updates

--- src/hooks/useQueryParams.js
+++
@@ -1,3 +0,0 @@
-import { useLocation } from 'react-router-dom'
-
-export const useQueryParams = () => new URLSearchParams(useLocation().search)
--- src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import favoriteReducer from './favoriteReducer'
-import favoritePlanetReducer from './favoritePlanetReducer'
-
-export default combineReducers({
-  favoriteReducer,
-  favoritePlanetReducer,
-})
